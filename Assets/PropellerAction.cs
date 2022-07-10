03401647c7e99d5b1b8396161691f0b15f8ae997
Hyunsoo Park <smwater130@naver.com>
2022-07-11 00:29:29 +0900

    헬리콥터 상승 구현

--- Assets/PropellerAction.cs
+++ Assets/PropellerAction.cs
@@ -3,17 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PropellerAction : MonoBehaviour
-{
+{ 
     public Rigidbody propeller;
-    public float angleY = 0f;
-    public float posY = 0;
+    public Rigidbody helicopter;
+    public float accelAngle = 0f;
+    public float posY = 1.7f;
     public float time = 0f;
     private float waitTime = 3f;
 
     // Start is called before the first frame update
     void Start()
     {
-            
+        propeller = GetComponent<Rigidbody>();
     }   
 
     // Update is called once per frame
@@ -21,17 +22,21 @@ public class PropellerAction : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.W) == true)
         {
-            angleY += 0.005f;
+            if (time < waitTime)
+            {
+                accelAngle += 0.005f;
+            }
+
             time += Time.deltaTime;
         }
 
-        propeller.transform.Rotate(0f, angleY % 360, 0f);
+        propeller.transform.Rotate(0f, accelAngle % 360, 0f);
 
         if (time >= waitTime)
         {
-            posY += 0.001f;
+            posY += 0.005f;
         }
 
-        propeller.AddForce(0f, posY, 0f);
+        helicopter.AddForce(0f, posY, 0f);
     }
 }
