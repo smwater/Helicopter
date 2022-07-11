220e26a277ab523c5c53a042218c8e65e85461cf
smwater <smwater130@naver.com>
2022-07-11 12:54:14 +0900

    시동 on/off로 변경

--- Assets/PropellerAction.cs
+++ Assets/PropellerAction.cs
@@ -7,6 +7,7 @@ public class PropellerAction : MonoBehaviour
     public Rigidbody helicopter;
 
     private bool isWKeyOff = true;
+    private bool isBoot = false;
 
     private float accelAngle = 0f;
     private const float addAngle = 0.005f;
@@ -14,8 +15,9 @@ public class PropellerAction : MonoBehaviour
     private float powerY = 0f;
     private const float speedY = 0.002f;
 
-    private float time = 0f;
+    private float timer = 0f;
     private const float waitTime = 2f;
+    private const float highSpeed = 1f;
 
     // Start is called before the first frame update
     void Start()
@@ -27,29 +29,28 @@ public class PropellerAction : MonoBehaviour
     void Update()
     {
         // 키 입력 감지
-        if (Input.GetKey(KeyCode.W) == true)
+        if (Input.GetKeyUp(KeyCode.R) == true)
+        {
+            // 시동 끄고 켜기
+            isBoot = !isBoot;
+        }
+
+        // 시동이 켜졌을 때
+        if (isBoot)
         {
             // 특정 시간에 도달하기 전까진 각도 변화를 증가시킴
-            if (time < waitTime)
+            if (timer < waitTime)
             {
                 accelAngle += addAngle;
             }
 
-            time += Time.deltaTime;
-
-            isWKeyOff = false;
+            timer += Time.deltaTime;
         }
 
-        // 키 입력이 끝났다면
-        if (Input.GetKeyUp(KeyCode.W) == true)
+        // 시동이 꺼졌을 때
+        if (!isBoot)
         {
-            isWKeyOff = true;
-        }
-
-        // 키를 입력하지 않을 때
-        if (isWKeyOff)
-        {
-            time = 0f;
+            timer = 0f;
 
             // 각도 변화를 감소시킴
             if (accelAngle > 0f)
@@ -65,13 +66,13 @@ public class PropellerAction : MonoBehaviour
         }
 
         // 특정 시간에 도달하면
-        if (time >= waitTime)
+        if (timer >= waitTime)
         {
-            // 높이 변화를 증가시킴
-            if (powerY <= 1f)
-            {
-                powerY += speedY;
-            }
+            //// 높이 변화를 증가시킴
+            //if (powerY <= highSpeed)
+            //{
+            //    powerY += speedY;
+            //}
         }
 
         // 위의 과정에서 나온 값을 vec에 반영
